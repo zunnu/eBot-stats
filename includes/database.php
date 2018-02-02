f10b22db98e16fb1be1b5a263c9c5140058a2440
Juhani Aronen <juhani.jamera@hotmail.com>
2018-02-02 22:59:00 +0200

    Add new flag images with better resolution, fixed some errors

--- includes/database.php
+++ includes/database.php
@@ -5,14 +5,15 @@ require_once("config.php");
 class Database {
 
 
-public $connection;
+	public $connection;
 
 	function __construct() {
 
-	    $this->open_db_connection();
+     $this->open_db_connection();
+	
 	}
 
-	public function open_db_connection(){
+	public function open_db_connection() {
 
 		$this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 		$set_char = $this->connection->set_charset("utf8");
@@ -26,20 +27,15 @@ public $connection;
 	public function query($sql) {
 
 		$result = $this->connection->query($sql);
-
 		$this->confirm_query($result);
 
 		return $result; 
 	}
 
 	private function confirm_query($result) {
-
 		if(!$result) {
-
 			die("Query Failed" . $this->connection->error);
-
 		}
-
 	}
 
 	public function escape_string($string) {
@@ -49,14 +45,19 @@ public $connection;
 		return $escaped_string;
 	}
 
+
+
 	public function the_insert_id() {
 
 		return $this->connection->insert_id;
 	}
 
+
+
+
+
 }  // End of Class Database
 
 
 $database = new Database();
-
 ?>
